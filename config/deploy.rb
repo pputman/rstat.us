a6874ed381b9f990993feaa89d261d35108b6191
Patrick <logik@dataromance.net>
2012-11-16 16:01:22 -0600

    removed airbrake

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,4 +1,4 @@
 server 'patrickputnam.managedmachine.com', :app, :web, :db, :primary => true
 
-        require './config/boot'
-        require 'airbrake/capistrano'
+#        require './config/boot'
+#        require 'airbrake/capistrano'
