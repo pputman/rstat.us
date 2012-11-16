3000afb26fbb918132a999662bf3b05a8406b6d2
Patrick <logik@dataromance.net>
2012-11-16 16:09:57 -0600

    readded airbrake

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,3 +2,5 @@ server 'patrickputnam.managedmachine.com', :app, :web, :db, :primary => true
 
 #        require './config/boot'
 #        require 'airbrake/capistrano'
+
+        require 'airbrake/capistrano'
