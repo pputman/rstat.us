cccc2611807b464ebff2eba4d30229d9e4acf4e2
Patrick <logik@dataromance.net>
2012-11-28 02:06:50 -0600

    removed mountaintop capistrano

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,11 +2,3 @@ server 'patrickputnam.managedmachine.com', :app, :web, :db, :primary => true
 
 #        require './config/boot'
         require 'airbrake/capistrano'
-require 'capistrano/mountaintop'
-set :campfire_options, {
-  :account => 'putman.patrick@gmail.com',
-  :room => 'Challengers',
-  :user => 'bender?',
-  :token => '012516e4c15edadfa16c371e33b8867e1c98',
-  :ssl => false
-}
