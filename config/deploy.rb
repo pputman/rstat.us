c3759642485ba6224e303993cd28dca593ed7628
Patrick <logik@dataromance.net>
2012-11-28 01:53:56 -0600

    edited mountaintop config

--- config/deploy.rb
+++ config/deploy.rb
@@ -5,7 +5,7 @@ server 'patrickputnam.managedmachine.com', :app, :web, :db, :primary => true
 require 'capistrano/mountaintop'
 set :campfire_options, {
   :account => 'putman.patrick@gmail.com',
-  :room => 'Challengers!',
+  :room => 'Challengers',
   :user => 'bender?',
   :token => '012516e4c15edadfa16c371e33b8867e1c98',
   :ssl => false
