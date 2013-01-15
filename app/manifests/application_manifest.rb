e290196f6e11e05761563a8234940bd6f7f9d941
Patrick Putnam <pputnam@Patricks-MacBook-Air.local>
2013-01-15 16:02:57 -0500

    added mysql to the application manifest

--- app/manifests/application_manifest.rb
+++ app/manifests/application_manifest.rb
@@ -22,6 +22,7 @@ class ApplicationManifest < Moonshine::Manifest::Rails
   # for details. To customize, remove this recipe and specify the components you want.
   recipe :default_stack
   recipe :mongodb
+  recipe :mysql
   # Add your application's custom requirements here
   def application_packages
     # If you've already told Moonshine about a package required by a gem with
